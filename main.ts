111a1f9134326eb4630a6f25c904ef660f2921f6
Yoshiya Hinosawa <stibium121@gmail.com>
2023-04-27 13:41:25 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import "https://deno-state-lib.deno.dev/polyfill.ts";
+
+const kv = Deno.openKv();
 
 serve(async (req: Request) => {
   const params = new URLSearchParams(new URL(req.url).search);
   const k = params.get("k");
   const v = params.get("v");
-  const db = await Deno.openDatabase();
-  await db.set(["users", k], { name: v });
-  const res = await db.get(["users", k]);
+  await kv.set(["users", k], { name: v });
+  const res = await kv.get(["users", k]);
   return new Response("Hello World " + JSON.stringify(res.value));
 });
