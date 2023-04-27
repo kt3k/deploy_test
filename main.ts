1a301617ac6749be1da712caa07732b509f9bf46
Yoshiya Hinosawa <stibium121@gmail.com>
2023-04-27 13:52:50 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
 
 const kv = await Deno.openKv();
 
+const zero = new Uint8Array();
+
 serve(async (req) => {
-/*
   const params = new URL(req.url).searchParams;
-  const k = params.get("k") ?? null;
-  const v = params.get("v") ?? null;
+  const k = params.get("k") ?? zero;
+  const v = params.get("v") ?? zero;
   await kv.set(["users", k], { name: v });
   const res = await kv.get(["users", k]);
-*/
-  return new Response("Hello World");
+  return new Response("Hello World " + JSON.stringify(res));
 });
